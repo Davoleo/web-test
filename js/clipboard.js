02836272ad214a9431c5472f1ece933a2678f5ca
Davoleo <davoleo2001@gmail.com>
2019-02-02 00:32:22 +0100

    Functions & Parameters
    + Refactored everything

--- js/clipboard.js
+++ js/clipboard.js
@@ -76,3 +76,63 @@ if ("blu") {alert("\"blu\" viene stampato")}
 //Undefined: Valore non assegnato
 //Null: forzato da noi, valore nullo
 //NaN: Not a Number, non si tratta di un valore numeric
+
+
+$(function () {
+    $("#surname").blur(format_text_box)
+});
+
+function format_text_box() {
+    var name = $("#name").val();
+    var surname = $("#surname").val();
+
+    if (isUpperCaseString(name))
+    {
+        $("#surname").val(surname.toUpperCase());
+    }
+    else if (isUpperCaseChar(name[0]))
+    {
+        //Strings in Javascript are read-only!
+        //surname[0] = "Z"
+        // ^^^ this would NOT work
+        surname = capitalize(surname);
+    }
+    else
+    {
+        if (isLowerCaseString(name))
+            surname = capitalize(surname);
+    }
+}
+
+function capitalize(string) {
+    var firstChar = string[0].toUpperCase();
+    var remainingString = string.toString().substring(1).toLowerCase();
+    return firstChar + remainingString;
+}
+
+function isLowerCaseString(string) {
+    return string === string.toString().toLowerCase();
+}
+
+function isUpperCaseChar(char) {
+    return String(char) === String(char).toUpperCase();
+}
+
+function isUpperCaseString(string) {
+    return string === string.toString().toUpperCase();
+}
+
+function showDate() {
+    document.getElementById('demo').innerHTML = Date();
+}
+
+function set_yellow_background() {
+    $("#eta").css("background-color", "yellow");
+}
+
+var test = "😪";
+console.log(test[0], test[1], test[0] + test[1] + "\n");
+
+console.log(test.charCodeAt(0), test.charCodeAt(1));
+console.log("\u{D83C}\u{DF77}");
+console.log("ciao".charCodeAt(0), "ciao".charCodeAt(1));
---
+++ js/theory/5-10.js
@@ -0,0 +1,28 @@
+/********************
+ *  Coded By Davoleo
+ ********************/
+
+/**
+ * Functions are introduced by the keyword 'function'
+ * Followed by the identifier of the function
+ * Followed by the parameters' parenthesis
+ * Followed by the curly brackets (AKA body AKA declaration of the function)
+ * Inside the body is the code that gets run once the function is called
+ *
+ * Calling a function: exampleFunction();
+ */
+function exampleFunction()
+{
+    //The alert function makes a popup come up on the page when called
+    //It takes one argument as parameter (The message of the popup)
+    alert("Ouch!!!");
+}
+
+function exampleFunction2(x) {
+    alert("I love " + x);
+}
+
+exampleFunction2("bacon");
+exampleFunction2("arte");
+exampleFunction2("JAVASCRIPT! jk I hate it");
+
