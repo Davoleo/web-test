2b81969c4bf1e34fe51fc66b2d20f8a716dab194
Davoleo <davoleo2001@gmail.com>
2019-02-04 15:59:56 +0100

    Multiple parameters | return statement | Nested functions | Recursion

--- js/theory/5-10.js
+++ js/theory/5-10.js
@@ -26,3 +26,57 @@ exampleFunction2("bacon");
 exampleFunction2("arte");
 exampleFunction2("JAVASCRIPT! jk I hate it");
 
+/**
+ * Multi-Parameter Function
+ * @param one (First Parameter)
+ * @param two (Second Parameter)
+ */
+function apples(one, two) {
+    document.write("<br>" + one + " is better than " + two + "<br>");
+}
+
+apples("Davoleo", "Matteo");
+
+/**
+ * This function returns a result by using the return statement
+ * @param a (Is the first number in the sum)
+ * @param b (Is the second number in the sum)
+ * @returns {*} (Is the value returned, AKA the sum, AKA a + b)
+ */
+function sum(a, b)
+{
+    return a + b;
+}
+
+document.write("the meaning of life is: " + sum(12, 30) + "<br>");
+
+/**
+ * Functions that call other functions
+ */
+function first() {
+    document.write("I am first brah<br>");
+}
+function second() {
+    document.write("Me not<br>");
+}
+
+function callFunctions() {
+    first();
+    second();
+}
+
+callFunctions();
+
+//Recursive call Example (Infinite loop)
+function lol0() {
+    document.write("0");
+    lol1();
+}
+
+function lol1() {
+    document.write(" 1 ");
+    lol0();
+}
+
+lol0();
+
