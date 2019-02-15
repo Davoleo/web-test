92198318628b3fe5b707fb15545206dc4c2725ae
Davoleo <davoleo2001@gmail.com>
2019-02-15 15:57:25 +0100

    Event handlers

---
+++ js/theory/22-27.js
@@ -0,0 +1,12 @@
+/********************
+ *  Coded By Davoleo
+ ********************/
+
+//Gets called by the event handler "onmouseover"
+function onHover() {
+    alert('lololol');
+}
+
+function onLoad() {
+    alert("yes babe, your ma babe, babe, don't you babe?")
+}
