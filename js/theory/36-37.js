73703e14c1c1578c8adafde22907bdf04e83f961
Davoleo <davoleo2001@gmail.com>
2019-03-14 18:08:45 +0100

    The Math class

---
+++ js/theory/36-37.js
@@ -0,0 +1,14 @@
+/********************
+ *  Coded By Davoleo
+ ********************/
+
+//The Math Class
+
+//Mathematical constants
+document.write(Math.PI + "<br>");
+document.write(Math.E + "<br><br>");
+
+let n = prompt("Enter a number", "");
+document.write("The Square Root of " + n + " is: " + Math.sqrt(n) + "<br>");
+
+//------------------------------------------
