d16df2ee553856ce615e3018e3ffc618ee301759
Davoleo <davoleo2001@gmail.com>
2019-04-02 20:48:36 +0200

    SQL - Database Management

---
+++ sql/manage_databases.sql
@@ -0,0 +1,12 @@
+-- creates a new database called test
+-- create database test;
+
+-- shows the current available databases
+show databases;
+-- selects database test
+use test;
+-- shows the currently available tables for the selected database
+show tables;
+
+-- deletes database test
+-- drop database test;
