0d04e4c08783647a855782c26e46445715a002d4
Davoleo <davoleo2001@gmail.com>
2019-06-04 16:31:05 +0200

    React: Rendering multiple components

--- js/react/1-5.jsx
+++ js/react/1-5.jsx
@@ -1,9 +1,27 @@
 
-const rootEl = document.getElementById("reactContainer");
+const rootEl = document.getElementById("⚛️");
 
 const spanElement = React.createElement('span', {}, 'SPAN!');
 // TYPE : Attributes : Children
 //const element = React.createElement("h1", {id: "header"}, "Hello World ", spanElement);
 const element = <h1 id="header">Hello World!!!</h1>;
-console.log(element);
-ReactDOM.render(element, rootEl);
+
+//React component
+let ExampleComponent = class extends React.Component{
+    render() {
+        return (
+            <div>
+                <h3>This is a simple react Component</h3>
+                <p>Some sort of bacon component</p>
+            </div>
+        );
+    }
+};
+
+//whenever you want to render multiple components you have to stick them together in a div tag
+ReactDOM.render(
+    <div>
+        <ExampleComponent/>
+        <ExampleComponent/>
+        element
+    </div>, rootEl);
