2269395b933280f53cc9be8e3e37f06c9082c5a4
Davoleo <davoleo2001@gmail.com>
2019-06-17 17:27:43 +0200

    Insert Data into a DB

---
+++ sql/insert_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO `test`.`colors`
+VALUES
+(2, "blue", "#0000FF"),
+(3, "green", "#00FF00"),
+(4, "red", "#FF0000"); 
+-- The values you want to insert - [Multiple insertion are separated by commas]
+
+INSERT INTO `test`.`colors`
+(`colorId`, `colorName`)
+-- The columns you want to insert values into (you can avoid them in case you insert data into every column)
+VALUES
+(2, "blue"); 
+-- The values you want to insert
