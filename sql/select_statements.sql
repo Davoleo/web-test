72bf0f0baa9e6825aa2d26c9e93a8084e2a94fa1
Davoleo <davoleo2001@gmail.com>
2019-06-19 19:05:26 +0200

    Selections

---
+++ sql/select_statements.sql
@@ -0,0 +1,11 @@
+-- Selects everything in the "colors" table
+SELECT * FROM colors;
+
+--Selects the colorId and colorName columns from the colors table
+SELECT colorId, colorName FROM colors;
+
+--Selects the colorId column from the colors table in the test DB
+SELECT colorId FROM `test`.`colors`;
+
+-- adds 3 to 1 (?)
+SELECT 1+3 FROM DUAL;
