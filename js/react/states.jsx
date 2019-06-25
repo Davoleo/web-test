502426508713f79084290ac59f9864e444717779
Davoleo <davoleo2001@gmail.com>
2019-06-25 20:28:33 +0200

    Fixed "this.setState undefined" | [fuck u React]

--- js/react/states.jsx
+++ js/react/states.jsx
@@ -4,7 +4,10 @@ let CheckBox = class extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {checked: true}
+        this.state = {checked: true};
+
+        //With React and ES6 - you need to bind the method to the class before referencing "this" in that method
+        this.changeCheckboxState = this.changeCheckboxState.bind(this);
     }
 
     changeCheckboxState() {
