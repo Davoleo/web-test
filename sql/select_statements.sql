b958af68792cba2902e4d4afb3ffed294877623e
Davoleo <davoleo2001@gmail.com>
2019-06-29 11:45:20 +0200

    Select distinct

--- sql/select_statements.sql
+++ sql/select_statements.sql
@@ -9,3 +9,9 @@ SELECT colorId FROM `test`.`colors`;
 
 -- adds 3 to 1 (?)
 SELECT 1+3 FROM DUAL;
+
+SELECT colorName FROM colors;
+SELECT DISTINCT colorName FROM colors;
+-- Distinct removes duplicates from your selection
+SELECT DISTINCT colorName, colorHex FROM colors;
+-- Removes duplicate (only if they have both the same colorName AND colorHex)
