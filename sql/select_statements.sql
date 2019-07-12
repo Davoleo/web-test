2ee8e3e36ace67d88b5ae3a82b12bda571d0d5d4
Davoleo <davoleo2001@gmail.com>
2019-07-12 15:54:11 +0200

    SQL - Complex Conditions (And/Or)

--- sql/select_statements.sql
+++ sql/select_statements.sql
@@ -31,3 +31,19 @@ SELECT colorId, colorHex FROM colors WHERE colorName="red";
 -- select all columns from the colors table where colorHex is null
 SELECT * FROM colors WHERE colorHex IS NULL;
 
+---------------- COMPLEX CONDITIONS ----------------------------------------------
+
+-- selects all the columns from the colors table where colorId is greater than 4 but also less than 18
+SELECT * FROM colors WHERE colorId > 4 AND colorId < 18;
+
+-- selects all the columns from the colors table where colorId is greater than 10 but also where colorName is equals to "red"
+SELECT * FROM colors WHERE colorId >= 10 AND colorName="red";
+
+-- selects all the columns from the colors table where colorId is greater than 10 or where colorName is equals to "red"
+SELECT * FROM colors WHERE colorId > 10 OR colorName="red";
+
+-- selects colorName from the colors table where colorId is greater than 15 or less than 17
+SELECT colorName FROM colors WHERE colorId > 15 OR colorId <= 17;   -- selects everything
+
+-- selects colorHex from the colors table where colorId is greater than 2 and also less or equals to 11 or when colorName is equals to "brown"
+SELECT colorHex FROM colors WHERE (colorId > 2 AND colorId <= 11) OR colorName="brown";
