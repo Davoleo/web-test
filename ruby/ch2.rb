1a94ec8f27e21d7d95fc95e762e88cb74c657c58
Davoleo <davoleo2001@gmail.com>
2019-09-08 13:41:30 +0200

    Ruby: Arrays

--- ruby/ch2.rb
+++ ruby/ch2.rb
@@ -2,6 +2,7 @@
 # Exceptions Handling
 # Strings
 # Symbols
+# Arrays
 
 
 def add_nums(num_1, num_2)
@@ -130,4 +131,49 @@ puts :davoleo.to_s
 puts :davoleo.class
 puts :davoleo.object_id
 
+puts "------------------------"
 
+#Arrays
+array_1 = Array.new
+array_2 = Array.new(5)  #5 slots with null default values
+array_3 = Array.new(5, "empty") #5 slots with "empty" default value
+array_4 = [1, "two", 3, 3.5]
+
+#Prints all the arrays' items in new lines
+puts array_1
+puts array_2
+puts array_3
+puts array_4
+
+puts "------------------------"
+
+# The third item of the fourth array
+puts array_4[2]
+puts array_4[2, 2].join(", ")
+puts array_4.values_at(0, 1, 3).join(", ")
+
+# Unshift add the parater at the beginning of the array
+# Shift removes the first array item
+array_4.unshift(0)
+array_4.shift()
+
+# push adds values to the end of the array
+# pop removes the last value from the array
+array_4.push(100, 200)
+array_4.pop()
+
+array_4.concat([10, 20, 30])
+puts "Array Size: " + array_4.size().to_s
+
+puts "Array Contains 100: " + array_4.include?(100).to_s
+
+puts "How Many 100s: " + array_4.count(100).to_s
+
+puts "Array is empty: " + array_4.empty?.to_s
+
+puts "------------------------"
+p array_4
+puts "------------------------"
+array_4.each do |value|
+    puts value 
+end
