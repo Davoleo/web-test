8ae58d0d633a909fa604dc29465faabb460f9e5a
Davoleo <davoleo2001@gmail.com>
2019-09-08 15:54:30 +0200

    JS: Stalker

---
+++ js/practice/stalker/stalker.js
@@ -0,0 +1,12 @@
+/********************
+ *  Coded By Davoleo
+ ********************/
+
+var username = prompt("What's your username?");
+var name = prompt("What's your name?");
+var surname = prompt("What's your surname?");
+var age = prompt("How old are you?");
+
+console.log(username + "'s Stolen Info");
+console.log("Full Name: " + name + " " + surname);
+console.log("Age: " + age);
