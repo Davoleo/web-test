45b90650691e21f7e57d7e17b99680c63f0f5f3a
Davoleo <davoleo2001@gmail.com>
2019-09-12 15:30:12 +0200

    JS: Age Conditionals

--- js/practice/age_calculator/age_calculator.js
+++ js/practice/age_calculator/age_calculator.js
@@ -3,6 +3,7 @@
  ********************/
 
 let age = prompt("How old are you?");
+const AGE = age;
 
 let days = 0;
 let hours = 0;
@@ -24,3 +25,26 @@ while (age > 0)
 
 alert("You lived about " + days + " days");
 alert("You lived about " + hours + " hours");
+
+if( AGE < 0)
+    alert("Error! this is an impossible age!");
+else if (AGE % Math.sqrt(AGE) === 0)
+{
+    alert("Perfect Square");
+}
+else if (AGE < 18)
+{
+    alert("You can't drive");
+}
+else if (AGE == 21)
+{
+    alert("Happy 21st Birthday");
+}
+else if (AGE % 2 !== 0)
+{
+    alert("Your age is odd");
+}
+else
+{
+    alert("You can drive");
+}
