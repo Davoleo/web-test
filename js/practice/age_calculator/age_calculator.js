bd31fa2856e9e91bcfdccbe20384042c5567d495
Davoleo <davoleo2001@gmail.com>
2019-09-13 17:13:26 +0200

    JS: Random Functions Practice

--- js/practice/age_calculator/age_calculator.js
+++ js/practice/age_calculator/age_calculator.js
@@ -26,6 +26,9 @@ while (age > 0)
 alert("You lived about " + days + " days");
 alert("You lived about " + hours + " hours");
 
+
+//Random Practice
+
 if( AGE < 0)
     alert("Error! this is an impossible age!");
 else if (AGE % Math.sqrt(AGE) === 0)
@@ -48,3 +51,41 @@ else
 {
     alert("You can drive");
 }
+
+console.log("-----------------------");
+
+function isEven(number) {
+    return number % 2 === 0;
+}
+console.log(isEven(4));
+console.log(isEven(21));
+console.log(isEven(68));
+console.log(isEven(333));
+
+console.log("-----------------------");
+
+function factorial(num) {
+    if (num > 0)
+        return  num * factorial(num - 1);
+    else
+        return 1;
+}
+
+console.log(factorial(5));
+console.log(factorial(2));
+console.log(factorial(0));
+console.log(factorial(10));
+
+console.log("-----------------------");
+
+function kebabToSnake(kebabString) {
+    while (kebabString.indexOf("-") > -1)
+    {
+        kebabString =  kebabString.replace("-", "_");
+    }
+    return kebabString;
+}
+
+console.log(kebabToSnake("hello-world"));
+console.log(kebabToSnake("bear-is-go-fast"));
+console.log(kebabToSnake("feelblu"));
