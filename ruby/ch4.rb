25eccddc43b84826397e845df108018f2ecfd909
Davoleo <davoleo2001@gmail.com>
2019-09-21 12:58:10 +0200

    Ruby: Enumerables

--- ruby/ch4.rb
+++ ruby/ch4.rb
@@ -1,5 +1,6 @@
 # Modules
 # Polymorphism
+# Enumerables
 
 require_relative "module_human"
 
@@ -53,3 +54,56 @@ end
 generic_bird = Bird.new
 generic_bird.tweet(Cardinal.new)
 generic_bird.tweet(Parrot.new)
+
+puts "--------------------------"
+
+# Enumerables
+class Menu include Enumerable
+
+    def each
+        yield "pizza"
+        yield "spaghetti"
+        yield "lasagne"
+        yield "salad"
+    end
+end
+
+#builds the class
+menu_options = Menu.new
+
+menu_options.each do |food|
+    puts "would you like some #{food}?"
+end
+
+puts "--------------------------"
+
+# check if an object is present in the enumerable
+p menu_options.find{|food| food = "pizza"}
+
+# selects all the items that are less or equal than 5
+p menu_options.select {|food| food.size <= 5}
+# negated select
+p menu_options.reject {|food| food.size <= 5}
+
+puts "--------------------------"
+# print out the first item
+p menu_options.first
+# print out the first and the second item
+p menu_options.take(2)
+puts "--------------------------"
+
+# print out anything except the first two items
+p menu_options.drop(2)
+puts "--------------------------"
+
+# depending on the alphabetical order
+p menu_options.min
+p menu_options.max
+puts "--------------------------"
+
+# sort an enumerable
+p menu_options.sort
+puts "--------------------------"
+
+# Print out everything in reverse order
+menu_options.reverse_each {|food| puts food}
