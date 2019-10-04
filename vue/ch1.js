0a1a5e934d49c85a5c64d91af387788e643ec98d
Davoleo <davoleo2001@gmail.com>
2019-10-04 18:47:15 +0200

    Vue: Hello World

---
+++ vue/ch1.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: "#vue_container",
+    data: {
+        title: "Hello World"
+    },
+    //template: "<button> {{title}} </button>"
+    //^^ Replaces the vue controlled element with this template
+});
+
+document.getElementById("greetings").addEventListener("click", function () {
+    app.title = "HOW YOU DARE TO CLICK ME!"
+})
