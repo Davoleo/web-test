faa1d4348674bf17cd34491a8dcaf22b4d53d7ac
Davoleo <davoleo2001@gmail.com>
2019-11-09 18:46:19 +0100

    JS: More DOM Stuff

--- js/theory/DOM/dom.js
+++ js/theory/DOM/dom.js
@@ -2,12 +2,13 @@
  *  Coded By Davoleo
  ********************/
 
-//DOM (Document Object Model) It represents the structure of the HTML coc elements
+//DOM (Document Object Model) It represents the structure of the HTML doc elements
+//The document is an extremely big JS Object made of all the data that is stored in HTML
 //a tag below you in hierarchy is called "child"
 //a tag above you in hierarchy is called "parent"
 //tags that are on the same level in hierarchy are called "siblings"
 
-/**
+/*
  * element.childNodes: returns an array of all the child nodes for the element
  * element.firstChild: returns the first child node of the element
  * element.lastChild: returns the last child node of the element
@@ -17,28 +18,61 @@
  * element.parentNode: returns the parent node of the element
  */
 
-/**
- * the property "innerHTML" is used to access the content of an element (the things between the tags)
- * @type {string}
- */
+
+//the property "innerHTML" is used to access the content of an element (the things between the tags)
 //document.body.innerHTML = "Some sample text";
 
-//Addresses the element that has the "testHeader" id
+//document.URL (returns the url of the page)
+//document.links (returns an HTMLCollection object of  all the links on the page)
+//document.body (returns the body's HTML)
+//document.head (returns the head's HTML)
+
+//DOM SELECTORS  ------------------------------------------------------------
+//Addresses the element that has the given id
 let elemFromId = document.getElementById("testHeader");
 
-//Addresses all the elements assigned to the "Main" class
+//Addresses all the elements assigned to the given class (Node List of html elements)
 let elemFromClass = document.getElementsByClassName("Main");
 
-//Addresses all the <p> (paragraph) elements
+//Addresses all the <p> (paragraph) elements (Node List of html elements)
 let elemFromTag = document.getElementsByTagName("P");
+
+//Uses CSS-styled selectors to define elements
+//if there are multiple elements it returns the first matching one
+//While querySelectorAll returns a Node List of all the matching html elements
+let querySelectedItem = document.querySelector("h1");
+let highlightedText = document.querySelector("#highlight");
+highlightedText.style.backgroundColor = "#FFFF00";
+
+var isBlue = false;
+setInterval(function () {
+    if (isBlue)
+        querySelectedItem.style.color = "#000000";
+    else
+        querySelectedItem.style.color = "#3498db";
+    isBlue = !isBlue;
+}, 500);
+
 
 function changeElements() {
     //This will show all the content of the body
     alert(elemFromClass[0].innerHTML);
     
-    //Change elements' inner HTML
-    elemFromTag[0].innerHTML = "AYYY";
-    elemFromId.innerHTML = "Hola!"
+    //Change elements' text content (does not include html tags)
+    elemFromTag[0].textContent = "AYYY";
+    elemFromId.textContent = "Hola!";
+
+    //classList is not an actual Array
+    //A fusion of classList.add and classList.remove
+    //Add a class to an HTML element if it doesn't have it
+    //if it has a class with that name it removes the class
+    elemFromId.classList.toggle("SampleClass");
+
+    var googleLink = document.querySelector("a");
+
+    //Getter and Setter for HTML Attributes
+    console.log(googleLink.getAttribute("href"));
+    googleLink.setAttribute("href", "https://davoleo.github.io");
 }
 
 function createElements() {
