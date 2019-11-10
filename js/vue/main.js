7d8b854f31380ce1e26b3ca00e1f7bff4d0fc90c
Davoleo <davoleo2001@gmail.com>
2019-11-10 20:40:53 +0100

    vue: v-if | v-else-if | v-else | v-show

--- js/vue/main.js
+++ js/vue/main.js
@@ -17,6 +17,62 @@ var App = Vue.component("App", {
     <button class="btn btn-primary" :disabled="isDisabled">Click</button>
     <p>Is this button Disabled? {{isDisabled ? " Yes" : " No"}}</p>
     <p>Complete name all caps: {{user.getFullName().toUpperCase()}}</p>
+    <hr>
+    
+    <!--    v-if only renders the element if the passed boolean is true -->
+    <div v-if="buttonEnabled">
+        <button class="btn btn-primary">Remove File</button>    
+    </div>
+    <div v-else>
+        User doesn't have permissions to remove files
+    </div>
+    <hr>
+    
+<!--  When using a template instead of a div to sorround controls, only the content of the container will be rendered  -->
+    <template v-if="buttonEnabled">
+        <button class="btn btn-dark">Remove File</button>    
+    </template>
+    <template v-else>
+        User doesn't have permissions to remove files
+    </template>
+    <hr>
+    
+<!--  If | Else If | Else  -->
+    <template v-if="type === 'A'">
+        A
+    </template>
+    <template v-else-if="type === 'B'">
+        B
+    </template>
+    <template v-else-if="type === 'C'">
+        C
+    </template>
+    <template v-else>
+        Not A/B/C
+    </template>
+    <hr>
+
+<!-- Form Example -->
+<!-- View doesn't re-render the input control automatically to make everything as lightweight as possible, if you want controls to render again you need to differentiate them by using a key -->
+    <form>
+        <div class="form-group">
+            <template v-if="loginType === 'username'">
+                <label>Username</label>
+                <input key="username" class="form-control" placeholder="Enter your username">                           
+            </template>
+            <template v-else>
+                <label>E-mail</label>
+                <input key="email" class="form-control" placeholder="Enter your e-mail address">            
+            </template>
+        </div>    
+    </form>
+    <button v-on:click="byUsername()" class="btn btn-primary">By Username</button>
+    <button v-on:click="byEmail()" class="btn btn-primary">By E-mail</button>
+    <hr>
+
+<!--    Lightweight alternative to v-if (v-show) -->
+    <button v-show="antistress" class="btn btn-danger">Panic Button</button>
+    
    </div>
   `,
     data() {
@@ -32,7 +88,17 @@ var App = Vue.component("App", {
                 getFullName: function () {
                     return this.firstName + " " + this.secondName;
                 }
-            }
+            },
+            buttonEnabled: true,
+            type: "B",
+            loginType: "username",
+            byUsername() {
+                this.loginType = "username"
+            },
+            byEmail() {
+                this.loginType = "email"
+            },
+            antistress: true
         };
     }
 });
