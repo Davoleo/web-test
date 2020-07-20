79f415c77becb4d4be33c2731fdb8db09661343c
Davoleo <davoleo2001@gmail.com>
2020-07-20 15:29:10 +0200

    Update web-tests repo for real lul
    Last time a sub-repo got in the way
    - Remove dist folder and update configuration files

--- js/vue/test-app/vue.config.js
+++ js/vue/test-app/vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/web-tests/js/vue/test-app/' : '/'
+    //publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
 }
