db41468d025fad04fea9b9e38150005a8c9cd0d8
Davoleo <davoleo2001@gmail.com>
2020-08-03 12:10:47 +0200

    Vue Todo-list

---
+++ js/vue/todo-list/src/components/AddTodo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input type="text" v-model="title" name="title" placeholder="Add Todo...">
+      <input class="button" type="submit" value="Submit">
+    </form>
+  </div>
+</template>
+
+<script>
+import uuid from 'uuid';
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    addTodo(event) {
+      event.preventDefault()
+      const newTodo = {
+        id: uuid.v4(),
+        title: this.title,
+        completed: false
+      };
+
+      this.$emit('add-todo', newTodo);
+      this.title = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+  }
+
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+
+  input[type="submit"] {
+    flex: 2;
+  }
+</style>
---
+++ js/vue/todo-list/src/components/TodoItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- Bind a class to the item conditionally -->
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="toggleComplete">
+      {{ todo.title }}
+      <button @click="$emit('delete-todo', todo.id)" class="delete-button">X</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    toggleComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .todo-item {
+    background-color: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #CCCCCC dotted;
+  }
+
+  .is-complete {
+    text-decoration: line-through;
+  }
+
+  .delete-button {
+    background-color: #FF0000;
+    color: #FFFFFF;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+    font-weight: bold;
+  }
+</style>
